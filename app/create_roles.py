b1caf88a46ac7128d1f0c1da7dca14297f1ea722
Савчук Андрей <a.savchuk@netrika.ru>
2025-02-13 23:43:06 +0700

    Изменен create_roles.py

--- app/create_roles.py
+++ app/create_roles.py
@@ -1,14 +1,8 @@
 # create_roles.py
-from flask import Flask
 from models import Role, db
+from app import app
 
 """Скрипт пока не  работает, но данные из него возможно использовать при создании ролей  через flask shell"""
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
 if __name__ == '__main__':
     with app.app_context():
@@ -24,23 +18,6 @@ if __name__ == '__main__':
         db.session.add(admin)
         db.session.add(teacher)
         db.session.add(staff)
-
         db.session.add(student)
         db.session.add(staff)
         db.session.commit()
-# def create_roles():
-#     admin = Role(id=1, name='Admin')
-#     teacher = Role(id=2, name='Teacher')
-#     staff = Role(id=3, name='Staff')
-#     student = Role(id=4, name='Student')
-#
-#     db.session.add(admin)
-#     db.session.add(teacher)
-#     db.session.add(staff)
-#     db.session.add(student)
-#
-#     db.session.commit()
-#     print("Roles created successfully!")
-#
-# # Function calling will create 4 roles as planned!
-# create_roles()
